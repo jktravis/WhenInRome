593a5a84fa9cff21ba43b161ff924c11d7a095c1
jktravis <joshua.travis@gmail.com>
2015-09-16 20:25:31 -0400

    Uncommented output. Added variables to address "additions." Still has trouble with 9s and higher two-digit numbers.

--- toRoman.js
+++ toRoman.js
@@ -1,6 +1,6 @@
 exports.convert = function (num)
 {
-    // console.log();
+    console.log();
     var romanNumerals = [
         ['I', 1], ['V', 5], ['X', 10], ['L', 50],
         ['C', 100], ['D', 500], ['M', 1000]
@@ -21,43 +21,45 @@ exports.convert = function (num)
             // is the number between two roman numerals?
             if (r >= romanNumerals[i][1] && r < romanNumerals[i + 1][1])
             {
-                // console.log('Value ' + r + ' is between ' + romanNumerals[i][1] + ' and ' + romanNumerals[i + 1][1]);
+                console.log('Value ' + r + ' is between ' + romanNumerals[i][1] + ' and ' + romanNumerals[i + 1][1]);
                 var symbolIter = r / romanNumerals[i][1];
-                // console.log('symbolIter = ' + r + ' / ' + romanNumerals[i][1] + ' = ' + symbolIter);
-                if (symbolIter % 1 === 0)
-                {
-                    // console.log('no remainder');
-                }
+                console.log('symbolIter = ' + r + ' / ' + romanNumerals[i][1] + ' = ' + symbolIter);
+
                 if (symbolIter <= 3)
                 {
                     if (symbolIter % 1 !== 0)
                     {
-                        // console.log('symbolIter % 1 !== 0: ' + (symbolIter % 1 !== 0));
-                        for (j = 0; j < Math.floor(symbolIter); j++)
+                        console.log('symbolIter % 1 !== 0: ' + (symbolIter % 1 !== 0));
+                        var diff = r - romanNumerals[i][1];
+                        var newIter = diff / romanNumerals[i - 1][i];
+                        console.log('diff: ' + diff);
+                        console.log('newIter: ' + newIter);
+                        for (j = 0; j < newIter; j++)
                         {
-                            // console.log('Adding digit: ' + romanNumerals[i - 1][0]);
+                            console.log('Adding digit: ' + romanNumerals[i - 1][0]);
                             converted.unshift(romanNumerals[i - 1][0]);
                         }
                     }
+
                     for (var j = 0; j < Math.floor(symbolIter); j++)
                     {
-                        // console.log('Adding digit: ' + romanNumerals[i][0]);
+                        console.log('Adding digit: ' + romanNumerals[i][0]);
                         converted.unshift(romanNumerals[i][0]);
                     }
                 }
                 else
                 {
-                    // console.log('symbolIter was greater than 3');
-                    // console.log('Adding digit: ' + romanNumerals[i + 1][0]);
+                    console.log('symbolIter was greater than 3');
+                    console.log('Adding digit: ' + romanNumerals[i + 1][0]);
                     converted.unshift(romanNumerals[i + 1][0])
                     converted.unshift(romanNumerals[i][0])
                 }
             }
         }
-        // console.log('---- Next digit ----');
+        console.log('---- Next digit ----');
         modVal *= 10;
     }
 
-    // console.log('##### Complete ####');
+    console.log('##### Complete ####');
     return converted.join('');
 }
